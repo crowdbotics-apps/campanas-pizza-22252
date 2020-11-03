37c68dc33079a881721663e7a32d3ef747e4da41
Crowdbotics <team@crowdbotics.com>
2020-11-03 13:12:41 +0000

    Update models

--- backend/campanas_pizza_22252/wsgi.py
+++ backend/campanas_pizza_22252/wsgi.py
@@ -11,6 +11,6 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'campanas_pizza_22252.settings')
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "campanas_pizza_22252.settings")
 
 application = get_wsgi_application()
